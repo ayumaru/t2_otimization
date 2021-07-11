c78ee216f77ff1810e97e27461a015a1b41247bf
maru <marllonwes@hotmail.com>
2021-07-11 20:16:11 -0300

    uma limpada no codigo

--- main.py
+++ main.py
@@ -4,122 +4,64 @@ import fileinput
 from itertools import permutations
 
 
-
-def tentativa2(matriz,n):
-    
-    vertices = [ i for i in range(1,n+1) ]
-    t_possibilidades = list(permutations(vertices))
-   
-    possibilidades = {} 
-
-    #mostra todas as possibilidades, agrupando por quem inicia
-    j = 1
-    for v in vertices:
-        tmp = []
-        for i in t_possibilidades:
-            if i[0] == v:
-                tmp.append(list(i))
-        possibilidades[v] = tmp
-    
-    possibilidades2 = [] 
-    for i in possibilidades[1]:
-        possibilidades2.append(i)
-
-    #print(possibilidades2)
-    
-    rmv = []
-    for i in possibilidades2:
-        if (matriz[0][ i[1]-1 ] == 0  ):
-            rmv.append(i)
-        if i[-1] != 1 and matriz[ i[-1]-1 ][0] == 0:
-            rmv.append(i)
-
-    
-    for i in rmv:
-        possibilidades2.remove( i )
-
-
-    rmv = []
-    for i in range(n):
-        print(matriz[i])
-
-
-
-    #print("pos>> \n",possibilidades2)
-
-    return 0
-
-
-
-
-
-
 def tentativa(matriz, n):
     vertices = [ i for i in range(1,n+1) ]   
-    possibilidades = { i: [] for i in vertices  } 
+    possibilidades = { i: [] for i in vertices } 
         
-    #todas possibilidades >3 ate tamanho n, agrupando em dicionario
+    #todas possibilidades >3 ate tamanho n
     for i in range(3,n+1):
-        for j in list( permutations(vertices, i)):
+        for j in list(permutations(vertices, i)):
             possibilidades[ j[0] ].append(j)
 
+    #pego apenas as possibilidades comecadas com 1 ( nodo inicial )
     possibilidades2 = [] 
     for i in possibilidades[1]:
         possibilidades2.append(list(i))
 
     
+    # retira todas as solucoes que o segundo vertice nao tem ligacao com o inicio, e o ultimo vertice nao tem caminho de volta para o inicio
     rmv = []
     for i in possibilidades2:
-        if ( matriz[0][ i[1]-1 ] == 0  ):
+        if matriz[0][ i[1]-1 ] == 0:
             rmv.append(i)
         if matriz[ i[-1]-1 ][0] == 0:
             rmv.append(i)
-
- 
     for i in rmv:
         if i not in possibilidades2: next
         else: possibilidades2.remove( i )
 
- 
-    rmv = []
+    # varre a matriz para verificar se existem vertices inuteis que nao tem como fazer parte da solucao
     vertice_inutil =[]
     for i in range(n):
         a = list(matriz[i]).count(0)
         if a > n-2:
             vertice_inutil.append(i)
 
-    for i in range(len(vertice_inutil)):
-        for j in possibilidades2:
-            if (vertice_inutil[i]+1) in j:
-                rmv.append(j)
-    
-    for i in rmv:
-        if i not in possibilidades2: next
-        else: possibilidades2.remove( i )
-
-    # print(possibilidades2,"\n ** \n")
+    rmv = []
+    #retira solucoes que possuem algum vertice inutil, ou seja, algum vertice que so e ligado por uma aresta com um unico vertice
+    if vertice_inutil:
+        for i in range(len(vertice_inutil)):
+            for j in possibilidades2:
+                if (vertice_inutil[i]+1) in j:
+                    rmv.append(j)
+        for i in rmv: 
+            if i not in possibilidades2: next
+            else: possibilidades2.remove( i )
 
     rmv = []
-    flag = False
-    for i in possibilidades2: # [ 1,2 ,3 ...]
-        #print("i before for: >>", i, "--\n")
+    #faz uma varredura buscando em cada possibilidade se os vertices do meio nao estao conectados com o proximo,
+    #se nao estiver para de percorrer aquela "arvore" e ja corta ela das solucoes possiveis
+    for i in possibilidades2: #[1,2,3...]
         for j in range(2,len(i)): 
-            #print("i>> ||", i ,"i[j]: >> ", i[j-1] ," || j: >>", i[j] ," || matriz[ ", i[j-1]-1," ][ ",i[j] - 1," ]: ",matriz[ i[j-1]-1 ][ i[j] - 1 ], "\n")
             if matriz[ i[j-1]-1 ][ i[j] - 1 ] == 0:
-                #print("entrou ^")
                 rmv.append(i)
                 break
-    
     for i in rmv:
         if i not in possibilidades2: next
         else: possibilidades2.remove( i )    
     
-
-    
-    # print(possibilidades2)
     encontra_sol(possibilidades2,matriz)
-    #return possibilidades2
-
+    
 
 
 def encontra_sol(caminhos, matriz):
@@ -185,35 +127,7 @@ def leitura():
 
 
 
-# def caixeiro_viajante(deslocamento, vertice, matriz, caminho, melhor_custo, melhor_caminho): #talvez C seja a matriz?
-#     if deslocamento == len(matriz):
-#         custo = caminho[0][ caminho[1] ]
-#         for v in range(2,len(caminho):
-#             custo +=  matriz[  caminho[v-1] ][caminho[v]]
 
-#         if custo < melhor_custo:
-#             melhor_custo = custo
-#             melhor_caminho = caminho
-
-#     if (deslocamento == 0):
-
-
-# caixeiro ( distancia l ):
-# global C_l onde eh o custo da distancia , distancia (l = 0,1, .. n-1)
-# if distancia l = nivel | vertices (?)
-#   entao: C <- custo ( vertices da solucao (?) [x0 ... x-1])
-#       if C < Custo otimo:
-#       entao: Custo otimo <- C
-#               Caminho otimo <- [x0 ... x-1]
-# if distancia l = 0
-#   entao C_l <- {0}
-#   se nao :
-#       if distancia l = 1
-#       entao: C_l <- {1...n-1}
-#        senao: C_l <- C_l-1 (custo anterior?) \ (divido ou exclusao?) { x_l-1 }
-#   para cada x pertencente ao C_l:
-#   faca: x_l <- x
-#         caixeiro(l+1) 
 
 
 
